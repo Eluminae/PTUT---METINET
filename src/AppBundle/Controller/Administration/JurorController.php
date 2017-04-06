4d968c3fcefbdcd5396b9725f1132c981482a55c
kokoal <alan.ridard@gmail.com>
2017-04-06 15:26:42 +0200

    Change folder all administrations controller files.

--- src/AppBundle/Controller/Administration/JurorController.php
+++ src/AppBundle/Controller/Administration/JurorController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Controller\Administration\UserManagerInterface;
 
 class JurorController extends Controller implements UserManagerInterface
 {
--- src/AppBundle/Controller/Administration/UserManagerInterface.php
+++ src/AppBundle/Controller/Administration/UserManagerInterface.php
@@ -6,7 +6,7 @@
  * Time: 16:13
  */
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Administration;
 
 
 use Symfony\Component\HttpFoundation\Request;
