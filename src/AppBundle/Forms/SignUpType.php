6a71e0913e3eb64111ed0834172caeafc8becd7f
Corentin Bouix <cbouix@clever-age.com>
2017-06-01 11:09:40 +0200

    - Clean back-end download system. - Clean some front - Registrer improvements

--- src/AppBundle/Forms/SignUpType.php
+++ src/AppBundle/Forms/SignUpType.php
@@ -3,12 +3,11 @@
 namespace AppBundle\Forms;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SignUpType extends AbstractType
 {
@@ -21,7 +20,14 @@ class SignUpType extends AbstractType
         $builder
             ->add('firstName', TextType::class)
             ->add('lastName', TextType::class)
-            ->add('password', RepeatedType::class)
-            ->add('signup', SubmitType::class, ['label' => 'S\'inscrire']);
+            ->add(
+                'password',
+                RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                ]
+            )
+            ->add('signup', SubmitType::class, ['label' => 'S\'inscrire'])
+        ;
     }
 }
