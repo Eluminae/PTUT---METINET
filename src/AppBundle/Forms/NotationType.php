79e8205588ac515ed2ed3bbac21b3edad861014d
Eluminae <eddie.barraco@idci-consulting.fr>
2017-06-06 10:26:30 +0200

    Campaign notation max value is now a NumberType instead of TestType.

--- src/AppBundle/Forms/NotationType.php
+++ src/AppBundle/Forms/NotationType.php
@@ -3,8 +3,8 @@
 namespace AppBundle\Forms;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class NotationType extends AbstractType
@@ -20,7 +20,7 @@ class NotationType extends AbstractType
                 'expanded' => true,
                 'multiple' => false,
             ))
-            ->add('markTypeNumber', TextType::class)
+            ->add('markTypeNumber', NumberType::class)
         ;
     }
 }
